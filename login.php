aaa7218073d32fe64632529696057bd9cb5b4d84
AARON  JOSEPH <77171398+aaronjosephz@users.noreply.github.com>
2025-07-15 11:49:45 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,172 +2,114 @@
 session_start();
 include "conn.php";
 
-
-function check($data){
-  $data= trim($data);
-  $data= htmlspecialchars($data);
-  $data= stripslashes($data);
+function check($data) {
+  global $con;
+  $data = trim($data);
+  $data = htmlspecialchars($data);
+  $data = stripslashes($data);
   $data = mysqli_real_escape_string($con, $data);
   return $data;
 }
 
-
-if(isset($_POST["login"])){
-  $uname = $_POST['username'];
+if (isset($_POST["login"])) {
+  $uname = check($_POST['username']);
   $pword = md5($_POST['password']);
-  //$rem = $_COOKIE['rememberme'];
-  $sql = "SELECT * FROM tenant WHERE u_name = '$uname' AND p_word = '$pword'";
-  $sql1 = "SELECT * FROM user WHERE u_name = '$uname' AND pword = '$pword'";
-  $query = mysqli_query($con, $sql);
-  $query1 = mysqli_query($con, $sql1);
-  $row = mysqli_fetch_assoc($query);
-  $row1 = mysqli_fetch_assoc($query1);
-  do {
-    $role = $row1['role'];
-    $row1 = mysqli_fetch_assoc($query1);
-  } while ($row1);
 
-  do{
-    $fname = $row['fname'];
+  $sqlTenant = "SELECT * FROM tenant WHERE u_name = '$uname' AND p_word = '$pword'";
+  $sqlUser = "SELECT * FROM user WHERE u_name = '$uname' AND pword = '$pword'";
 
-    $lname = $row['lname'];
+  $queryTenant = mysqli_query($con, $sqlTenant);
+  $queryUser = mysqli_query($con, $sqlUser);
 
-    $stat = $row['status'];
+  $rowTenant = mysqli_fetch_assoc($queryTenant);
+  $rowUser = mysqli_fetch_assoc($queryUser);
 
-    $id = $row['tenant_id'];
-    $sql2 = "SELECT * FROM contract WHERE tenant_id = '$id'";
-    $query2 = mysqli_query($con, $sql2);
-    $row1 = mysqli_fetch_assoc($query2);
+  $role = $rowUser['role'] ?? null;
+  $fname = $rowTenant['fname'] ?? '';
+  $lname = $rowTenant['lname'] ?? '';
+  $stat = $rowTenant['status'] ?? null;
+  $tenant_id = $rowTenant['tenant_id'] ?? null;
 
-    do{
-      $end_date = $row1['end_day'];
-      $h_id = $row1['house_id'];
-      $row1 = mysqli_fetch_assoc($query2);
-    }while ($row1);
-    $row = mysqli_fetch_assoc($query);
+  // Contract Info
+  if ($tenant_id) {
+    $contractResult = mysqli_query($con, "SELECT * FROM contract WHERE tenant_id = '$tenant_id'");
+    $contract = mysqli_fetch_assoc($contractResult);
+    $end_date = $contract['end_day'] ?? null;
+    $house_id = $contract['house_id'] ?? null;
+  }
 
-  }while ($row);
+  if (mysqli_num_rows($queryTenant) === 1 || mysqli_num_rows($queryUser) === 1) {
+    $_SESSION['username'] = $uname;
+    echo "<script>alert('Welcome $fname $lname!');</script>";
+    echo '<style>body{display:none;}</style>';
 
-
-
-  if ((mysqli_num_rows($query) == 1) || (mysqli_num_rows($query1) == 1)) {
-
-
-
-    if($role == "Administrator"){
-      $_SESSION['username'] = $uname;
-      echo "<script type='text/javascript'>alert('Welcome $uname!');</script>";
-      echo '<style>body{display:none;}</style>';
+    if ($role === "Administrator") {
       echo '<script>window.location.href = "admin_home.php";</script>';
-
-    }
-    elseif ($role == "Manager") {
-      $_SESSION['username'] = $uname;
-      echo "<script type='text/javascript'>alert('Welcome $uname!');</script>";
-      echo '<style>body{display:none;}</style>';
+    } elseif ($role === "Manager") {
       echo '<script>window.location.href = "manager_home.php";</script>';
-    }
-    else {
-
+    } else {
       if ($stat == 0) {
-        $_SESSION['username'] = $uname;
-        echo "<script type='text/javascript'>alert('Welcome $fname $lname!');</script>";
-        echo '<style>body{display:none;}</style>';
         echo '<script>window.location.href = "initial_payment.php";</script>';
-      }elseif ($stat == 1) {
-        $_SESSION['username'] = $uname;
-        echo "<script type='text/javascript'>alert('Welcome $fname $lname!');</script>";
-        echo '<style>body{display:none;}</style>';
-        echo '<script>window.location.href = "home.php";</script>';
-      }elseif ($stat == 2) {
-        $_SESSION['username'] = $uname;
-        echo "<script type='text/javascript'>alert('Welcome $fname $lname!');</script>";
-        echo '<style>body{display:none;}</style>';
+      } elseif ($stat == 1) {
+        if (date('Y-m-d') > $end_date) {
+          mysqli_query($con, "UPDATE tenant SET status = '3' WHERE tenant_id = '$tenant_id'");
+          mysqli_query($con, "UPDATE contract SET status = 'Inactive' WHERE status = 'Active' AND tenant_id = '$tenant_id'");
+          mysqli_query($con, "UPDATE house SET status = 'Empty' WHERE house_id = '$house_id'");
+          echo "<script>alert('Your contract has expired. Please renew to continue.');</script>";
+          echo '<script>window.location.href = "renew_contract.php";</script>';
+        } else {
+          echo '<script>window.location.href = "home.php";</script>';
+        }
+      } elseif ($stat == 2) {
         echo '<script>window.location.href = "waiting.php";</script>';
-      }elseif((date('Y-m-d') > $end_date) && $stat == 1){
-        $sql3 = "UPDATE tenant SET status = '3' WHERE tenant_id = '$id'";
-        mysqli_query($con, $sql3);
-        $sql5 = "UPDATE contract SET status ='Inactive' WHERE status = 'Active' AND tenant_id = '$id'";
-        mysqli_query($con, $sql5);
-        $sql5 = "UPDATE house SET status ='Empty' WHERE house_id = '$h_id'";
-        mysqli_query($con, $sql5);
-        $_SESSION['username'] = $uname;
-        echo "<script type='text/javascript'>alert('Welcome $fname $lname! Your contract has expired. To access the system please renew the contract.');</script>";
-        echo '<style>body{display:none; color:red;}</style>';
-        echo '<script>window.location.href = "renew_contract.php";</script>';
-
-      }elseif ($stat == 3) {
-        $_SESSION['username'] = $uname;
-        echo "<script type='text/javascript'>alert('Welcome $fname $lname! Your contract has expired. To access the system please renew the contract.');</script>";
-        echo '<style>body{display:none;}</style>';
+      } elseif ($stat == 3) {
+        echo "<script>alert('Your contract has expired. Please renew to continue.');</script>";
         echo '<script>window.location.href = "renew_contract.php";</script>';
       }
     }
     mysqli_close($con);
-    $uname = "";
-
-
-  }else {
-    echo "<script style = 'color:red;'> alert('Incorrect Username or Password!!!')</script>";
+  } else {
+    echo "<script>alert('Incorrect Username or Password!');</script>";
   }
-
-
 }
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>USER LOGIN</title>
   <link rel="icon" href="rent.ico">
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,700,900" rel="stylesheet">
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
-
 <body style="background-image: linear-gradient(#4e73df, white);">
-
   <div class="container">
-
-    <!-- Outer Row -->
     <div class="row justify-content-center">
-
       <div class="col-xl-10 col-lg-12 col-md-9">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block"><img src="house.jpg" alt="Rental House" width="500" height="530" style="opacity:0.6;"></div>
+              <div class="col-lg-6 d-none d-lg-block">
+                <img src="house.jpg" alt="Rental House" width="500" height="530" style="opacity:0.6;">
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4"><b><b>GITANGA DUPLEXES</b></b><br/><br/>UserLogin</h1>
+                    <h1 class="h4 text-gray-900 mb-4"><b>GITANGA DUPLEXES</b><br/>User Login</h1>
                   </div>
-                  <form class="user" action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST">
+                  <form class="user" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" name="username" aria-describedby="emailHelp" value="<?php echo @$uname; ?>" placeholder="Username">
+                      <input type="text" name="username" class="form-control form-control-user" placeholder="Username" value="<?php echo @$uname; ?>">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" name="password" placeholder="Password">
+                      <input type="password" name="password" class="form-control form-control-user" placeholder="Password">
                     </div>
-
-                    <input class="btn btn-primary btn-user btn-block" type="submit" name="login" value="Login">
+                    <input type="submit" name="login" class="btn btn-primary btn-user btn-block" value="Login">
                   </form>
-                    <hr>
+                  <hr>
                   <div class="text-center">
                     <a class="small" href="forgot-password.php">Forgot Password?</a>
                   </div>
@@ -179,30 +121,18 @@ if(isset($_POST["login"])){
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
-
   </div>
-
-
   <script>
-    if ( window.history.replaceState ) {
-      window.history.replaceState( null, null, window.location.href );
+    if (window.history.replaceState) {
+      window.history.replaceState(null, null, window.location.href);
     }
   </script>
-
-  <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
   <script src="js/sb-admin-2.min.js"></script>
-
 </body>
-
 </html>
+
